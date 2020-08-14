b3466f057d4b1d985b725460c0d001645dc26afa
Guilherme Illescas <gui.illescas@gmail.com>
2020-08-14 12:51:34 -0300

    Simple operators and loops

---
+++ Aula02-Classes/Calc.java
@@ -0,0 +1,10 @@
+public class Calc {
+  static int multiplica(int a, int b) {
+    return a * b;
+  }
+
+  public static void main (String[] args) {
+    int z = multiplica(3, 4);
+    System.out.println(z);
+  }
+}
---
+++ Aula02-Classes/Repeticao.java
@@ -0,0 +1,13 @@
+public class Repeticao {
+  public static void main (String[] args) {
+    int i = -2;
+    while ( i> 0) {
+      i --;
+      if (i == 3)
+        break;
+        System.out.println("i = " + i);
+    };
+    System.out.println("Fim");
+  }
+}
+
