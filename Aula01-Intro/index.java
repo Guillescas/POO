090a380d80a97aea05f9dce47e6199b358ccd71d
Guilherme Illescas <gui.illescas@gmail.com>
2020-08-14 10:10:30 -0300

    Firts commit

---
+++ Aula01-Intro/index.java
@@ -0,0 +1,23 @@
+class Test {
+  public static void main( String[] args ) {
+    // Printing words in Java (main == função)
+
+    // int k = 0;           // k é uma variável do tipo inteiro
+    // short s = 0;         // s é uma variável do tipo inteiro curto
+    // long z = 0;          // z é uma variável do tipo inteiro longo
+    // byte w = 0;          // w é uma variável do tipo inteiro, 8 bits
+    // boolean b = false;   // b é uma variável do tipo boolean
+    // float f = 0;         // f é uma variável do tipo real simples
+    // double d = 0;        // d é uma variável do tipo real duplo
+
+  public static void main (String[] args) {
+    System.out.println("Helo World!");
+  }
+}
+
+class Aula {
+  public static void main(String[] args) {
+    
+  }
+}
+
