9d93f34ecb0941fa2098e849b45383e56ce71f50
Guilherme Illescas <gui.illescas@gmail.com>
2020-09-07 15:14:03 -0300

    Calling other files and await response, completed

--- Trabalho3/Banco.java
+++ Trabalho3/Banco.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 public class Banco {
   public static void main(String[] args) {
-    Cliente guilherme = new Cliente('Guilherme', 11987);
-    Cliente elisa = new Cliente('Elisa', 11456);
+    // Scanner handleChoice = new Scanner(System.in);
+    // Scanner handleInput = new Scanner(System.in);
+    // int option;
+
+    Cliente guilherme = new Cliente("Guilherme", 84276);
+    Cliente elisa = new Cliente("Elisa", 11456);
+    Cliente laura = new Cliente("Laura", 95621);
 
     guilherme.operar();
-    elisa.operar();
+
+    // System.out.print("Por favor, digite o número da sua conta:\n");
+
+    // System.out.print("Escolha uma opção digitando o número correspondente: ");
+    // option = handleChoice.nextInt();
   }
 }
